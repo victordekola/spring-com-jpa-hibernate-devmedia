1bb7c1cd35d926887f74466af33cb4815aff102c
Victor Gomes <victordekola@gmail.com>
2020-05-28 20:17:59 -0300

    Atualização Git

---
+++ src/main/java/br/com/devmedia/curso/domain/Usuario.java
@@ -0,0 +1,128 @@
+package br.com.devmedia.curso.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 50)
+	@NotBlank
+	@Size(min = 3, max = 50)
+	private String nome;
+
+	@Column(nullable = false, length = 50)
+	@NotBlank
+	@Size(min = 3, max = 50, message = "Campo requerido entre {min} e {max} caracteres.")	
+	private String sobrenome;
+
+	@Column(name = "data_nascimento", nullable = false)
+	@NotNull(message = "O campo 'data de nascimento' é requerido.")
+	@DateTimeFormat(iso = ISO.DATE)
+	private LocalDate dtNascimento;
+
+	@Column(name = "tipo_sexo", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private TipoSexo sexo;
+	
+	public Usuario() {
+		super();
+	}	
+
+	public Usuario(Long id, String nome, String sobrenome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+	}	
+	
+	public Usuario(Long id, String nome, String sobrenome, LocalDate dtNascimento) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.dtNascimento = dtNascimento;
+	}
+	
+	public Usuario(Long id, String nome, String sobrenome, LocalDate dtNascimento, TipoSexo sexo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.dtNascimento = dtNascimento;
+		this.sexo = sexo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public LocalDate getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(LocalDate dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+
+	public TipoSexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(TipoSexo sexo) {
+		this.sexo = sexo;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Usuario usuario = (Usuario) o;
+		return Objects.equals(id, usuario.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + "]";
+	}
+	
+	
+}
